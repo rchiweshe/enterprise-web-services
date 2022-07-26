9a11011c7abbfe8ea5e3c998027991340d3bafda
takudzwakamango <takudzwa.kamango@eeconeet.co.zw>
2022-07-26 14:56:57 +0200

    added marker interfaces

---
+++ service-products/service-promotions/src/main/java/zw/co/econet/servicepromotions/repository/config/DataConfig.java
@@ -0,0 +1,14 @@
+package zw.co.econet.servicepromotions.repository.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import zw.co.econet.servicepromotions.domain.DomainMarkerInterface;
+import zw.co.econet.servicepromotions.repository.RepositoryMarkerInterface;
+
+@Configuration
+@EnableJpaRepositories(basePackageClasses = {RepositoryMarkerInterface.class})
+@EntityScan(basePackageClasses = {DomainMarkerInterface.class})
+public class DataConfig {
+
+}
